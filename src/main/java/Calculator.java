df08064d04b25a8bca5a835a6ee2657d581a1128
Qiguang Chu <chuqiguang@gmail.com>
2021-05-25 23:30:30 +0800

    finish one

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,6 +7,13 @@
 
 public class Calculator {
     public int compute(int num1, int num2, char c) {
-        return num1+num1;
+        switch (c) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
