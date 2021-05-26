6806fdcf3c9694cc37ee2f51817f88d078845762
Qiguang Chu <chuqiguang@gmail.com>
2021-05-26 08:28:50 +0800

    new four calculator

---
+++ src/main/java/Multi.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021
+ * @Author: Qiguang Chu
+ * @Email: chuqiguang@sina.com
+ * @LastModified:2021-05-26T08:23
+ */
+
+public class Multi implements Computable {
+
+    @Override
+    public int compute(int num1, int num2) {
+        return num1 * num2;
+    }
+}
